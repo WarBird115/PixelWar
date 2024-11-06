b2d60d30389aa4a32832356b6a033b6c45270e7c
WarBird115 <valterkiislar@gmail.com>
2024-11-07 01:58:32 +0200

    Updated HTML and JS for Pixel Wars

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
 import { getDatabase, ref, set, onValue, remove } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
 
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCjcSLUJsjQWmITFt3gQCul9BcNs1ABTpA",
   authDomain: "pixelwarnew.firebaseapp.com",
@@ -12,9 +13,11 @@ const firebaseConfig = {
   measurementId: "G-9VH085RDE1"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
+// Canvas setup
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const cooldownTimer = document.getElementById('cooldown-timer');
@@ -34,14 +37,18 @@ const passwords = [
   "I1jK2", "L3mN4", "O5pQ6", "R7sT8", "U9vW0",
   "X1yZ2", "A3bC4", "D5eF6", "G7hI8", "J9kL0",
   "M1nO2", "P3qR4", "S5tU6", "V7wX8", "Y9zA0",
-  "B1cD2", "E3fG4", "H5iJ6", "K7lM8", "N9oP0",
-  "Q1rS2", "T3uV4", "W5yZ6", "A7bC8", "D9eF0",
+  "B1cD2", "E3fG4", "H5iJ6", "K7mN8", "N9oP0",
+  "Q1rS2", "T3uV4", "W5yZ6", "Z7aB8", "C9dE0",
+  "F1gH2", "I3jK4", "L5mN6", "O7pQ8", "R9sT0",
+  "U1vW2", "X3yZ4", "A5bC6", "D7eF8", "G9hI0",
+  "J1kL2", "M3nO4", "P5qR6", "S7tU8", "V9wX0",
+  "Y1zA2", "B3cD4", "E5fG6", "H7iJ8", "K9lM0",
+  "N1oP2", "Q3rS4", "T5uV6", "W7yZ8", "Z9aB0",
+  "C1dE2", "F3gH4", "I5jK6", "L7mN8", "O9pQ0",
+  "R1sT2", "U3vW4", "X5yZ6", "A7bC8", "D9eF0",
   "G1hI2", "J3kL4", "M5nO6", "P7qR8", "S9tU0",
   "V1wX2", "Y3zA4", "B5cD6", "E7fG8", "H9iJ0",
-  "K1lM2", "N3oP4", "Q5rS6", "T7uV8", "W9yZ0",
-  "Z1aB2", "C3dE4", "F5gH6", "I7jK8", "L9mN0",
-  "O1pQ2", "R3sT4", "U5vW6", "X7yZ8", "A9bC0",
-  "D1eF2", "G3hI4", "J5kL6", "M7nO8", "P9qR0"
+  "K1lM2", "N3oP4", "Q5rS6", "T7uV8", "W9yZ0"
 ];
 
 // Set canvas size
@@ -149,7 +156,7 @@ canvas.addEventListener('contextmenu', (e) => {
 });
 
 // Admin login
-const adminPassword = "Itsameamario1";
+const adminPassword = "The0verseer";
 const userPasswordField = document.getElementById('userPassword');
 const loginButton = document.getElementById('loginButton');
 const clearCanvasButton = document.getElementById('clearCanvasButton');
