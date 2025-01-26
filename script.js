025d872c66b2213c97736211def44de7a1c7453c
WarBird115 <valterkiislar@gmail.com>
2025-01-26 22:25:30 +0200

    Password change

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ let currentPassword = ''; // Store the password from Firebase here
 let isUserAuthenticated = false; // Track user authentication status
 
 // Temporary password for regular users
-const regularUserPassword = "HappyN3wY3ar"; // Set your temporary password here
+const regularUserPassword = "Hrhw32"; // Set your temporary password here
 
 // Function to load the weekly password from Firebase
 function loadWeeklyPassword() {
